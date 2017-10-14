2e5f27a22c353d92ab381e4f886362db77d7b315
Kyle Webster <kyle1234webster@gmail.com>
2017-10-14 10:11:24 -0600

    Update GraphApp.java

--- GraphApp.java
+++ GraphApp.java
@@ -1,17 +1,14 @@
-
 public class GraphApp {
 	public static void main(String[] args){
-		
+
 		Graph theGraph = new Graph();
 		theGraph.displayVertex(0);
 		theGraph.printMatrix("adjacency_matrix.txt");
-		System.out.println("Depth First Search Visits: ");
+		System.out.println("Depth First Search Visits and Minimum Spanning Tree: ");
 		theGraph.depthFirstSearch();
 		System.out.println("");
 		System.out.println("Breadth First Search Visits: ");
 		theGraph.breadthFirstSearch();
-		System.out.println("Minimum Spanning Tree: ");
-		theGraph.minSpanTree();
 		System.out.println("");
 	}
 }
